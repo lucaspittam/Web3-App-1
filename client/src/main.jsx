abd67d33d1c72b3d2ad41da2a81c037ab6b4e78e
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-07-31 23:36:37 -0400

    error updates with react dom importing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+
 import App from './App'
+import { TransactionsProvider } from './context/TransactionContext';
 import './index.css'
-import { TransactionProvider } from './context/TransactionContext';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <TransactionProvider>
-
-
+  <TransactionsProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </TransactionProvider>
+  </TransactionsProvider>
 )
